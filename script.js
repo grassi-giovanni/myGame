343c6dc97c25d56270f269c7632e462b62212529
grassi-giovanni <grassi.giovanni@iisgalvanimi.edu.it>
2021-12-16 22:05:25 +0000

    versione1.3-movimenti+(inizio)bordi

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
+//pezzi
 var myGamePiece;
+
+//Bordi
+var myGameSide;
+var myGameSide2;
+var myGameSide3;
+var myGameSide4;
+
 
 function startGame() {
     myGamePiece = new component(40, 20, "green", 150, 150);
+
+    myGameSide = new component(10, 1000, "red", 10, 10);
+    myGameSide2 = new component(10, 1000, "red", 1490, 10);
+    myGameSide3 = new component(1490, 10, "red", 10, 10);
+    myGameSide4 = new component(1490, 10, "red", 10, 660);
     myGameArea.start();
 }
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 1000;
-        this.canvas.height = 500;
+        this.canvas.width = 1510;
+        this.canvas.height = 670;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
-    },
-    clear : function() {
+        window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        })
+    }, 
+    clear : function(){
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
+
+   
 
 function component(width, height, color, x, y) {
     this.width = width;
@@ -36,25 +57,24 @@ function component(width, height, color, x, y) {
         this.y += this.speedY;        
     }    
 }
-
+hsp = 3;
 function updateGameArea() {
     myGameArea.clear();
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;    
+    myGamePiece.speedX = hsp * ((myGameArea.key == 39) - (myGameArea.key == 37));
+    myGamePiece.speedY = hsp * ((myGameArea.key == 40) - (myGameArea.key == 38));
+    
+    if (myGamePiece.x + hsp < myGameSide.x + myGameSide.width) {myGamePiece.speedX = 3; }
+    if (myGamePiece.x + 3 >  myGameSide3.x - myGameSide3.width) {myGamePiece.speedX = -3; }
+    if (myGamePiece.y + hsp < myGameSide2.y - myGameSide2.width) {myGamePiece.speedY = 3; }
     myGamePiece.newPos();    
     myGamePiece.update();
-}
 
-function moveup() {
-    myGamePiece.speedY -= 1; 
-}
+    //Carica i bordi
+    myGameSide.update();
+    myGameSide2.update();
+    myGameSide3.update();
+    myGameSide4.update();
 
-function movedown() {
-    myGamePiece.speedY += 1; 
-}
-
-function moveleft() {
-    myGamePiece.speedX -= 1; 
-}
-
-function moveright() {
-    myGamePiece.speedX += 1; 
 }
